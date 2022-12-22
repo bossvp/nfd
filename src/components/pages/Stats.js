a181454998702ed01f97f0757a9df80112625c04
John <cjmgalind@gmail.com>
2022-12-21 22:50:30 -0500

    stats

--- src/components/pages/Stats.js
+++ src/components/pages/Stats.js
@@ -1,28 +1,55 @@
-import React from 'react'
-import Buttons from '../stats-components/Buttons'
-import TableLucky from '../stats-components/TableLucky'
-import TableRewards from '../stats-components/TableRewards'
-import TableWeek from '../stats-components/TableWeek'
+import React from "react";
+import Buttons from "../stats-components/Buttons";
+import TableLucky from "../stats-components/TableLucky";
+import TableRewards from "../stats-components/TableRewards";
+import TableWeek from "../stats-components/TableWeek";
 import Footer from "../Footer";
+import { useState } from "react";
 function Stats() {
+  const [eTabs, setTabs] = useState({
+    Week: true,
+    Rewards: false,
+    Lucky:false,  
+  });
+
+  function seteo(prop) {
+    const tabs = {
+      Week: false,
+      Rewards: false,
+      Lucky:false,  
+    };
+
+    setTabs({ ...tabs, [prop]: true });
+  }
+
+  let btnStyles = {
+    background: "#C74C39",
+  };
   return (
-    <div  className="background__stats">
-        <div className="container-fluid container__stats" >
+    <div className="background__stats">
+      <div className="container-fluid container__stats">
         <div className="container__title">
           <h3 className="container__title-title">stats</h3>
-        
-        <div className="row">
-<Buttons/>
-{/* <TableWeek/> */}
-{/* <TableRewards/> */}
-<TableLucky/>
-        </div>
-        </div>
-        </div>
 
-        <Footer />
+          <div className="row">
+            <Buttons seteo={seteo} btnStyles={btnStyles} eTabs={eTabs} />
+            { eTabs.Week &&
+              <TableWeek/> 
+            }
+            {eTabs.Rewards &&
+              <TableRewards/>
+            }
+            {eTabs.Lucky &&
+              <TableLucky />
+            }
+            
+          </div>
+        </div>
+      </div>
+
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Stats
+export default Stats;
