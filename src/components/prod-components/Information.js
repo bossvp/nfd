0766d6c785c602b2aa995564a96329f7a65045d2
John <cjmgalind@gmail.com>
2023-03-14 19:21:32 -0500

    deal

--- src/components/prod-components/Information.js
+++ src/components/prod-components/Information.js
@@ -5,66 +5,68 @@ import iconInfo from '../../img/info.png'
 
 function Information({eSelect,seteoWindow,setText, eText, color = "#B06A26", btnInfo}) {
   return (
-    <div className="col-12 col-md-3 d-flex flex-column" >
+    <div className="col-12 col-md-3 d-flex box_info__prod" >
 
-        <div className='d-flex justify-content-center align-items-center' style={{marginBottom:"2.5rem"}}> 
-            <img src={iconInfo} alt=""  style={{width:"2rem", height:"2rem" , marginRight:"2rem"}}/>
-            <h1
-              className="text-white text-uppercase text-center fw-bold m-1 d-flex"
-              style={{ margin: "200px 0px" }}
-            >
-           
-              info {eText.tittle}
-            </h1>
+        <div>
+          <div className='d-flex justify-content-center align-items-center' style={{marginBottom:"2.5rem"}}> 
+              <img src={iconInfo} alt=""  style={{width:"2rem", height:"2rem" , marginRight:"2rem"}}/>
+              <h1
+                className="text-white text-uppercase text-center fw-bold m-1 d-flex"
+                style={{ margin: "200px 0px" }}
+              >
+             
+                info {eText.tittle}
+              </h1>
+          </div>
+          
+          
+          
+          <h4 className="text-uppercase text-white fw-bold text-center p-0 m-0">
+            Assets Multiplier
+          </h4>
+          <p className="text-white text-center  " style={{ marginLeft: "70px" }}>
+            0.5%
+          </p>
+          
+          <h4 className="text-uppercase text-white fw-bold text-center p-0 m-0">
+            Random
+          </h4>
+          <p className="text-white text-center  " style={{ marginLeft: "70px" }}>
+            After slot are lack, we add <br /> the random bonus
+          </p>
+          
+          <h4 className="text-uppercase text-white fw-bold text-center p-0 m-0">
+            total multiplier
+          </h4>
+          <p className="text-white text-center  " style={{ marginLeft: "70px" }}>
+            0.5% (+/-10%)
+          </p>
+          
+          <table className="table__layout mb-4" style={{  }}>
+          <tbody>
+          <tr>
+              <td>neutral bonus</td>
+              <td>|</td>
+              <td>0%</td>
+            </tr>
+            <tr>
+              <td>sativa bonus</td>
+              <td>|</td>
+              <td>0%</td>
+            </tr>
+            <tr>
+              <td>Hybrid Bonus</td>
+              <td>|</td>
+              <td>8</td>
+            </tr>
+            <tr>
+              <td>indica bonus</td>
+              <td>|</td>
+              <td>10%</td>
+            </tr>
+          </tbody>
+          </table>
         </div>
-
-        
-
-        <h4 className="text-uppercase text-white fw-bold text-center p-0 m-0">
-          Assets Multiplier
-        </h4>
-        <p className="text-white text-center  " style={{ marginLeft: "70px" }}>
-          0.5%
-        </p>
-
-        <h4 className="text-uppercase text-white fw-bold text-center p-0 m-0">
-          Random
-        </h4>
-        <p className="text-white text-center  " style={{ marginLeft: "70px" }}>
-          After slot are lack, we add <br /> the random bonus
-        </p>
-
-        <h4 className="text-uppercase text-white fw-bold text-center p-0 m-0">
-          total multiplier
-        </h4>
-        <p className="text-white text-center  " style={{ marginLeft: "70px" }}>
-          0.5% (+/-10%)
-        </p>
-
-        <table className="table__layout mb-4" style={{  }}>
-        <tbody>
-        <tr>
-            <td>neutral bonus</td>
-            <td>|</td>
-            <td>0%</td>
-          </tr>
-          <tr>
-            <td>sativa bonus</td>
-            <td>|</td>
-            <td>0%</td>
-          </tr>
-          <tr>
-            <td>Hybrid Bonus</td>
-            <td>|</td>
-            <td>8</td>
-          </tr>
-          <tr>
-            <td>indica bonus</td>
-            <td>|</td>
-            <td>10%</td>
-          </tr>
-        </tbody>
-        </table>
 
         <div className="row button__prod" style={{marginTop:"3rem"}}>
           <div className="col-12 d-flex flex-column justify-content-center align-items-center">
@@ -93,7 +95,7 @@ function Information({eSelect,seteoWindow,setText, eText, color = "#B06A26", btn
           </div>
         </div>
 
-      </div>
+    </div>
   )
 }
 
--- src/components/season-components/MissionCards.js
+++ src/components/season-components/MissionCards.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../blend-components/Card'
 import bike from '../../img/season/bike.png'
 import Phi from "../../img/phi201.png";
 import PadLock from "../../img/padlock.png";
+import ModalText from '../Modals/ModalText';
+import ModalCards from '../Modals/ModalCards';
+
 
 function MissionCards() {
+  const [modalText, setModalText] = useState(false);
+  const [MSPhi, setMSPhi] = useState(false);
   return (
     <div  className="col-12 col-md-5 ">
 
@@ -12,34 +17,47 @@ function MissionCards() {
 
             <Card img={Phi} text={"PHS ( 28657 )" } color={"rgb(254, 175, 0)"}/>
             <Card img={bike} text={"BIKE ( 001 )" } color={"rgb(255,255,255)"}/>
-            
-            <div className="card__prod ">
+
+            {MSPhi && (
+                          <ModalCards
+                            state={MSPhi}
+                            setstate={setMSPhi}
+                            img={Phi}
+                  />
+            )}
+             
+            <div className="card__prod " onClick={() => setMSPhi(!MSPhi)}>
                 <img src={PadLock} style={{width:"8em",height:"8em"}}  />
                 <button style={{width:"10em"}} className="btn btn-dark btn-lg btn-block position-absolute bottom-0 m-2 w-9em" >SELECT BAG</button>
               </div>
 
-              <div className="card__prod ">
+              <div className="card__prod " onClick={() => setMSPhi(!MSPhi)}>
                 <img src={PadLock} style={{width:"8em",height:"8em"}}  />
                 <button style={{width:"10em"}} className="btn btn-dark btn-lg btn-block position-absolute bottom-0 m-2 w-9em" >SELECT BAG</button>
               </div>
 
-            <div className="card__prod__locked_slot">
+              {modalText && (
+                  <ModalText state={modalText} setState={setModalText} />
+                )}
+
+
+            <div className="card__prod__locked_slot" onClick={() => setModalText(!modalText)}>
                 <p className="fw-bold pt-2">LOCKED SLOT</p>
                 <img src={PadLock} alt="" />
                 
               </div>
-              <div className="card__prod__locked_slot">
+              <div className="card__prod__locked_slot" onClick={() => setModalText(!modalText)}>
                 
                 <p className="fw-bold pt-2">LOCKED SLOT</p>
                 <img src={PadLock} alt="" />
             
               </div>
-              <div className="card__prod__locked_slot">
+              <div className="card__prod__locked_slot" onClick={() => setModalText(!modalText)}>
                 
                 <p className="fw-bold pt-2">LOCKED SLOT</p>
                 <img src={PadLock} alt="" />
               </div>
-              <div className="card__prod__locked_slot">
+              <div className="card__prod__locked_slot" onClick={() => setModalText(!modalText)}>
                 
                 <p className="fw-bold pt-2">LOCKED SLOT</p>
                 <img src={PadLock} alt="" />
